fe00e440c96b81e349ee869b7eff6426f1999723
Thinh <thinhtabingo@gmail.com>
2025-12-26 06:09:20 +0700

    Upload PHT7,8,9

---
+++ PHT_8/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\SinhVienController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/sinhvien', [SinhVienController::class, 'index'])
+    ->name('sinhvien.index');
+
+Route::post('/sinhvien', [SinhVienController::class, 'store'])
+    ->name('sinhvien.store');
